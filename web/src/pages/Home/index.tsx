0d8d1b933ee4718b4193aede5937268a6e42f070
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-06 17:03:53 -0300

    add: Home and initial layout

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg'
+import { FiSearch } from 'react-icons/fi'
+
+import './styles.css';
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return <div id="page-home">
+    <div className="content">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+      </header>
+
+      <main>
+        <h1>Seu marketplace de coleta de resíduos.</h1>
+        <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+        <a href="./pages">
+          <span>
+            <FiSearch />
+          </span>
+
+          <strong>Pesquisar pontos de coleta</strong>
+        </a>
+      </main>
+    </div>
+  </div>;
+}
+
+export default Home;
