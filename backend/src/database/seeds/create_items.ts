f32597af38e5cb685ec7b74b5be59f7d234e1a55
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-06 20:10:13 -0300

    fix: Seed image name

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
   return await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papeis e Papelão', image: 'papeis-papelão.svg' },
+    { title: 'Papeis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
     { title: 'Óleo de cozinha', image: 'oleo.svg' },
