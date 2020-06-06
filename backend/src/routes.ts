b5beb94173d4e7ac9ab7853bd48ac81abb4e1c26
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-05 21:46:05 -0300

    Return serialized items on route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
+import knex from './database/connection';
+
 const routes = Router();
 
-routes.get('/customers', (request, response) => {
-  return response.json({ ok: true });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return response.json(serializedItems);
 });
 
 export default routes;
