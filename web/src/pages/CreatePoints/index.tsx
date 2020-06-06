f4e51e2f7ea8f46b42a45d49e3b2d16cb53dc621
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-06 18:28:20 -0300

    add: Static map

--- web/src/pages/CreatePoints/index.tsx
+++ web/src/pages/CreatePoints/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
 import logo from '../../assets/logo.svg'
 import './styles.css';
 
@@ -60,6 +62,14 @@ const CreatePoints: React.FC = () => {
           <h2>Endereço</h2>
           <span>Selecione o endereço no mapa</span>
         </legend>
+
+        <Map center={[-23.4553304, -46.4489193]} zoom={15}>
+          <TileLayer 
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-23.4553304, -46.4489193]} />
+        </Map>
 
         <div className="field-group">
           <div className="field">
