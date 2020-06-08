bc2ae2278778b703b2e64c97d9d4097a4878c642
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-08 17:59:49 -0300

    add: Home page

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons'
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Home: React.FC = () => {
   return (
-    <Text>Home</Text>
+    <ImageBackground 
+      source={require('../../assets/home-background.png')} 
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton onPress={() => {}} style={styles.button}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
   )
 }
 
