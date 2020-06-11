2e8c094595b217cc81a19b99794235c98321d3e6
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-10 23:27:39 -0300

    add: Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-
+import React, { useCallback } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+ 
 const Detail: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [])
+
   return (
-    <Text style={styles.pointName}>Detail</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name='arrow-left' size={20} color='#34cb79' />
+          </TouchableOpacity>
+
+          <Image style={styles.pointImage} source={{uri: 'https://blog.rcky.com.br/wp-content/uploads/2020/01/minimercado.jpg'}} />
+
+          <Text style={styles.pointName}>Mercado do João</Text>
+          <Text style={styles.pointItems}>Lâmpadas, Óleo de cozinha</Text>
+          
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>São Paulo, SP</Text>
+          </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name='whatsapp' size={20} color='#fff' />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name='mail' size={20} color='#fff' />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -58,6 +95,7 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: '#999',
     paddingVertical: 20,
+    paddingBottom: 0,
     paddingHorizontal: 32,
     flexDirection: 'row',
     justifyContent: 'space-between'
