929a43162ca5b2855574e087bc2c976aad94c26d
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-11 14:49:24 -0300

    chore: Config image upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
@@ -7,10 +10,11 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create)
+routes.post('/points', upload.single('image'), pointsController.create)
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
