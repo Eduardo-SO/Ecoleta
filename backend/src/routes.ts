efc1d4b53e6dfce72425926462fe5a987e5fd912
Eduardo SO <eduardo.sool07@gmail.com>
2020-06-11 17:22:26 -0300

    chore: Create point route validation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -14,8 +15,27 @@ const upload = multer(multerConfig);
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create)
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
+routes.post(
+  '/points',
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().max(2).required(),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false,
+  }), 
+  pointsController.create
+);
+
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
